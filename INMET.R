516683024c26d268b7083b00553a5c7e7b55a04a
Carlo <carlo.demusis@gmail.com>
2023-03-05 17:51:26 -0400

    Add files via upload

---
+++ INMET.R
@@ -0,0 +1,109 @@
+library(readr)
+library(lubridate)
+
+# Carga de dados
+inmet <- read_delim("inmet Campo Verde_A912_2019.csv", 
+                    delim = ";",
+                    escape_double = FALSE, 
+                    trim_ws = TRUE)
+
+colnames(inmet)[19] <- 'P'
+inmet$P <- as.numeric(gsub(",", ".", inmet$P))
+
+colnames(inmet)[6] <- 'UR'
+inmet$UR <- inmet$UR/10
+
+colnames(inmet)[15] <- 'v'
+inmet$v <- as.numeric(gsub(",", ".", inmet$v))
+
+colnames(inmet)[2] <- 'hora'
+
+# Exclui NAs
+inmet <- inmet[!is.na(inmet$UR),]
+
+# Converte texto para data 
+inmet$Data <- as.Date(inmet$Data, format = "%d/%m/%Y")
+
+# Extrai o mês e ano
+inmet$mes <- month(inmet$Data)
+inmet$ano <- year(inmet$Data)
+inmet$dia <- day(inmet$Data)
+
+# Seleciona dados para 13:00
+inmet13 <- inmet[inmet$hora == '1300',]     
+inmet13 <- inmet13[, c('dia', 'mes', 'ano', 'UR', 'P', 'v')]
+
+# Agrupa dados por precipitação
+inmetDia <- aggregate(P ~ ano + mes + dia, data = inmet13, FUN = sum)
+inmet13 <- inmet13[, c('dia', 'mes', 'ano', 'UR', 'v')]
+
+# Mesclar os dois dataframes
+inmetMesc <- merge(inmet13, inmetDia, by = c('dia', 'mes', 'ano'))
+inmetMesc <- inmetMesc[order(inmetMesc$ano, inmetMesc$mes, inmetMesc$dia),]
+
+# Fator de propagação
+fp <-function(v) {
+  return (exp(0.04*v))
+}
+
+# Calcula o FMA
+inmetMesc$FMA <- NA
+
+for (i in 1:nrow(inmetMesc)) {
+  # Primeiro Registro
+  if (i == 1) {
+    if (inmetMesc[i, 'P'] < 2.5) {
+      inmetMesc[i, 'FMA'] <- 100/inmetMesc[i, 'UR']*fp(inmetMesc[i, 'v'])
+    } else {
+      if (inmetMesc[i, 'P'] > 12.9) {
+        inmetMesc[i, 'FMA'] <- 0
+      } else {
+        inmetMesc[i, 'FMA'] <- NA
+      }
+    }
+  } else {
+    if (inmetMesc[i, 'P'] < 2.5) {
+      inmetMesc[i, 'FMA'] <- 100/inmetMesc[i, 'UR']*fp(inmetMesc[i, 'v'] + inmetMesc[i-1, 'FMA'])
+    } else {
+      if (inmetMesc[i, 'P'] < 5.0) {
+        inmetMesc[i, 'FMA'] <- 100/inmetMesc[i, 'UR']*fp(inmetMesc[i, 'v'] + .70*inmetMesc[i-1, 'FMA'])
+      } else {
+        if (inmetMesc[i, 'P'] < 10.0) {
+          inmetMesc[i, 'FMA'] <- 100/inmetMesc[i, 'UR']*fp(inmetMesc[i, 'v'] + .40*inmetMesc[i-1, 'FMA'])
+        } else {
+          if (inmetMesc[i, 'P'] < 13.0) {
+            inmetMesc[i, 'FMA'] <- 100/inmetMesc[i, 'UR']*fp(inmetMesc[i, 'v'] + 20 * inmetMesc[i-1, 'FMA'])
+          } else {
+            inmetMesc[i, 'FMA'] <- 0
+          }
+        }
+      }
+    }
+  }
+}
+
+inmetMesc$GP <- NA
+
+# Grau de perigo
+gp <- function(FMA) {
+  print(FMA)
+  if (FMA <= 3) {
+    return('Nulo')
+  } else {
+    if (FMA <= 8) {
+      return('Pequeno')
+    } else {
+      if (FMA <= 14) {
+        return('Médio')
+      } else {
+        if (FMA <= 24) {
+          return('Alto')
+        } else {
+          return('Muito alto')
+        }
+      }
+    }
+  }
+}
+
+
